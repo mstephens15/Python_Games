c59d1f703a7ed6cc529aa0ed94ae68c7850af4a5
Mitch Stephens <mstephens@sandiego.edu>
2020-06-03 16:42:28 -0700

    Window is up and running, basic init in place

--- Workout Game/Model 0.1.py
+++ Workout Game/Model 0.1.py
@@ -1,1 +1,25 @@
-One more test
+import pygame
+from pygame import mixer  # For background music and sounds
+
+# Done every time
+pygame.init()
+
+# Screen
+screen = pygame.display.set_mode((800,600))
+
+# Background
+
+# Background Music
+
+# Title and Icon
+
+# Window Loop
+running = True
+while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((100,150,180))
+
+
+        pygame.display.update()
