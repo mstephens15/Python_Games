ea02ab532511f334ab585557c6a3d8567525da06
Mitchell Stephens <mstephens@sandiego.edu>
2020-06-03 16:11:59 -0700

    Redownloaded packages and changed environments because folder name changed

--- Space Invaders/venv/bin/activate.fish
+++ Space Invaders/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Mitch/Desktop/Python Games/Space Invaders/venv"
+set -gx VIRTUAL_ENV "/Users/Mitch/Desktop/Python_Games/Space Invaders/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
