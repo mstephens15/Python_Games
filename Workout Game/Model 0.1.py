9c935c39e691dedc918c405b584447bcd6c2c5d2
Mitch Stephens <mstephens@sandiego.edu>
2020-06-05 20:30:26 -0700

    Version 0.1: Added complete button, replaces the pad but only momentarily

--- Workout Game/Model 0.1.py
+++ Workout Game/Model 0.1.py
@@ -8,6 +8,14 @@ pygame.init()
 # Screen
 screen = pygame.display.set_mode((800,600))
 
+# Colors
+bright_green = (0, 255, 0)
+green = (0, 155, 0)
+black = (0, 0, 0)
+
+# Constants
+smallText = pygame.font.Font("freesansbold.ttf",16)
+
 # Background
 
 # Background Music
@@ -17,10 +25,13 @@ pygame.display.set_caption("Dudley Model 0.1")
 icon = pygame.image.load("cotton-pad.png")
 pygame.display.set_icon(icon)
 
+# Mission Complete Button
+
+
 # Level 1
 level1Img = pygame.image.load("cotton-pad.png")
 level1X = 368
-level1Y = 500
+level1Y = 450
 
 # Level 2
 level2Img = pygame.image.load("cotton-pad.png")
@@ -49,25 +60,52 @@ for i in range(num_trees):
 def draw_level(x, y):
     screen.blit(level1Img, (x, y))
 
-
 def draw_trees(x, y, i):
     screen.blit(treeImg[i], (x, y))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def draw_button(msg, x, y, w, h, inactive, active):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    replaceImg = pygame.image.load("tree.png")
+    def replace(x, y):
+        screen.blit(replaceImg, (x, y))
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, active, (x,y,w,h))
+        if click[0] == 1:
+            replace(level1X, level1Y)
+            pygame.display.update()
+    else:
+        pygame.draw.rect(screen, inactive, (x,y,w,h))
+
+        # Button Text
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
 
 # Window Loop
 running = True
 while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill((100,150,180))
 
-        draw_level(level1X, level1Y)
-        draw_level(level2X, level2Y)
+    screen.fill((100,150,180))
 
-        for i in range(num_trees):
-            draw_trees(tree_leftX[i], tree_leftY[i], i)
-            draw_trees(tree_rightX[i], tree_rightY[i], i)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        black = 0, 0, 0
-        pygame.draw.line(screen, black, (level1X + 32,level1Y), (level2X + 32,level2Y + 64), 5)
-        pygame.display.update()
+    draw_level(level1X, level1Y)
+    draw_level(level2X, level2Y)
+    draw_button("Complete", 355, 550, 90, 30, green, bright_green)
+
+    for i in range(num_trees):
+        draw_trees(tree_leftX[i], tree_leftY[i], i)
+        draw_trees(tree_rightX[i], tree_rightY[i], i)
+
+    # Lines between levels
+    pygame.draw.line(screen, black, (level1X + 32,level1Y), (level2X + 32,level2Y + 64), 5)
+
+    pygame.display.update()
