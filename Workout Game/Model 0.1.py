4d35b09686830a374a7039f3ebcccbbc23f2559c
Mitch Stephens <mstephens@sandiego.edu>
2020-06-06 15:47:47 -0700

    Version 0.2: Added sprite, button still doesn't work

--- Workout Game/Model 0.1.py
+++ Workout Game/Model 0.1.py
@@ -16,6 +16,11 @@ black = (0, 0, 0)
 # Constants
 smallText = pygame.font.Font("freesansbold.ttf",16)
 
+left = pygame.K_LEFT
+right = pygame.K_RIGHT
+up = pygame.K_UP
+down = pygame.K_DOWN
+
 # Background
 
 # Background Music
@@ -25,8 +30,17 @@ pygame.display.set_caption("Dudley Model 0.1")
 icon = pygame.image.load("cotton-pad.png")
 pygame.display.set_icon(icon)
 
-# Mission Complete Button
+# Sprite
+guy = pygame.image.load("weak.png")
+guyX = 50
+guyY = 425
+guyX_change = 0
+guyY_change = 0
 
+    # Sprite Movement
+width = 60
+height = 60
+vel = 5
 
 # Level 1
 level1Img = pygame.image.load("cotton-pad.png")
@@ -60,8 +74,14 @@ for i in range(num_trees):
 def draw_level(x, y):
     screen.blit(level1Img, (x, y))
 
+def draw_level2(x, y):
+    screen.blit(level2Img, (x, y))
+
 def draw_trees(x, y, i):
     screen.blit(treeImg[i], (x, y))
+
+def draw_sprite(x, y):
+    screen.blit(guy, (x, y))
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -69,16 +89,9 @@ def text_objects(text, font):
 
 def draw_button(msg, x, y, w, h, inactive, active):
     mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    replaceImg = pygame.image.load("tree.png")
-    def replace(x, y):
-        screen.blit(replaceImg, (x, y))
 
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(screen, active, (x,y,w,h))
-        if click[0] == 1:
-            replace(level1X, level1Y)
-            pygame.display.update()
     else:
         pygame.draw.rect(screen, inactive, (x,y,w,h))
 
@@ -87,18 +100,24 @@ def draw_button(msg, x, y, w, h, inactive, active):
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     screen.blit(textSurf, textRect)
 
+def replace(x, y):
+    transparent = (0,0,0,0)
+    replaceImg = pygame.image.load("tree.png")
+    click = pygame.mouse.get_pressed()
+    if click[0] == 1:
+        level1Img.fill(transparent)
+        screen.blit(replaceImg, (x, y))
+
 # Window Loop
 running = True
 while running:
-
     screen.fill((100,150,180))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     draw_level(level1X, level1Y)
-    draw_level(level2X, level2Y)
+    draw_level2(level2X, level2Y)
     draw_button("Complete", 355, 550, 90, 30, green, bright_green)
 
     for i in range(num_trees):
@@ -107,5 +126,43 @@ while running:
 
     # Lines between levels
     pygame.draw.line(screen, black, (level1X + 32,level1Y), (level2X + 32,level2Y + 64), 5)
+
+    # for event in pygame.event.get():
+    #     if event.type == pygame.MOUSEBUTTONDOWN:
+    #         print("works")
+    #         level1Img.fill((0,0,0,0))
+    #         replace(level1X, level1Y)
+    # replace(level1X, level1Y)
+
+
+    # Movement
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == left and guyX > vel:
+                guyX_change = -5
+            if event.key == right and guyX < 500-width-vel:
+                guyX_change = 5
+            if event.key == up and guyY > vel:
+                guyY_change = -5
+            if event.key == down and guyY < 500 - height - vel:
+                guyY_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == left or event.key == right or event.key == up or event.key == down:
+                playerX_change = 0
+
+    guyX += guyX_change
+    guyY += guyY_change
+
+    if guyX <= 0:
+        guyX = 0
+    elif guyX >= 736:
+        guyX = 736
+
+    if guyY <= 0:
+        guyY = 0
+    elif guyY >= 550:
+        guyY = 550
+
+    draw_sprite(guyX, guyY)
 
     pygame.display.update()
