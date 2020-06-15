c6ab3de33e2aad341268159a1b7912ca714803fa
Mitch Stephens <mstephens@sandiego.edu>
2020-06-15 12:50:39 -0700

    First

--- Dudley_A/main.py
+++ Dudley_A/main.py
@@ -1,1 +1,25 @@
 import pygame
+
+# Initialization
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+
+# Title and Icon
+pygame.display.set_caption("Dudley 1.0")
+icon = pygame.image.load("sprites/pokemon.png")
+pygame.display.set_icon(icon)
+
+# Main Loop
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Display fill and update
+
+    screen.fill((100,150,180))
+    pygame.display.update()
+
+pygame.quit()
