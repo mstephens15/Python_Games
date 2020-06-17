15fc2f26c6cd8db019ca2dbb9404b300093f0176
Mitch Stephens <mstephens@sandiego.edu>
2020-06-16 18:41:15 -0700

    Small adjustments, right before leaving to move Derek

--- Dudley_A/grid.py
+++ Dudley_A/grid.py
@@ -1,28 +1,31 @@
 import pygame
+from constants import *
 
 # Tiles
 Dirt = 0
 Grass = 1
 Water = 2
+Pad = 3
 
 # Dictionary of pictures
 Textures = {
-    Dirt: pygame.image.load('./sprites/dirt.png'),
-    Grass: pygame.image.load('./sprites/grass.png'),
-    Water: pygame.image.load('./sprites/water.png'),
+    Dirt: pygame.image.load('./textures/dirt.png'),
+    Grass: pygame.image.load('./textures/grass.png'),
+    Water: pygame.image.load('./textures/water.png'),
+    Pad: pygame.image.load('./textures/square.png')
 }
 
 Grid = [
-    [Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass],
-    [Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt],
-    [Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass],
-    [Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt],
-    [Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass],
-    [Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt],
-    [Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water],
-    [Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt, Dirt],
-    [Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water, Water],
-    [Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass,Grass]
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,3,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 ]
 
 Tilesize = 50
