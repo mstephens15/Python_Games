76c9582b56f068cfda436d02a698666e7da1ba8f
Mitch Stephens <mstephens@sandiego.edu>
2020-06-21 10:51:55 -0700

    Added smooth movement to the sprite

--- Dudley_A/main.py
+++ Dudley_A/main.py
@@ -28,11 +28,12 @@ class Game:
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()  # creating the sprites group
         self.walls = pg.sprite.Group()        # creating the walls group
-        self.player = Player(self, 10, 10)
         for row, tiles in enumerate(self.map_data):  # Enumerate gets item and index number
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     Wall(self, col, row)
+                if tile == "P":
+                    self.player = Player(self, col, row)
 
     def run(self):
         # game loop - set self.playing = False to end the game
@@ -71,14 +72,6 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     self.quit()
-                if event.key == pg.K_LEFT:
-                    self.player.move(dx=-1)  # Can leave out dy because it is 0 by default
-                if event.key == pg.K_RIGHT:
-                    self.player.move(dx=1)
-                if event.key == pg.K_UP:
-                    self.player.move(dy=-1)
-                if event.key == pg.K_DOWN:
-                    self.player.move(dy=1)
 
     def show_start_screen(self):
         pass
--- Dudley_A/sprite.py
+++ Dudley_A/sprite.py
@@ -10,26 +10,53 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(yellow)
         self.rect = self.image.get_rect()      # required
-        self.x = x                             # keeping track of where the sprite is
-        self.y = y                             # keeping track of where the sprite is
+        self.vx, self.vy = 0, 0                # Velocity
+        self.x = x * TILESIZE                            # keeping track of where the sprite is
+        self.y = y * TILESIZE                            # keeping track of where the sprite is
 
-    # Lets us move in one direction; by default, it is zero. If i move dx = 1, then dy = 0 still, so it will only go
-    # right or left
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071    # 1 / sqrt(2)
+            self.vy *= 0.7071
 
-    def move(self, dx=0, dy=0):
-        if not self.collide_with_walls(dx, dy):
-            self.x += dx
-            self.y += dy
-
-    def collide_with_walls(self, dx=0, dy=0):
-        for wall in self.game.walls:
-            if wall.x == self.x + dx and wall.y == self.y + dy:
-                return True
-        return False
+    def collide_with_walls(self, dir):
+        if dir == 'x':  # if the collision is horizontal, i.e. from x
+            hits = pg.sprite.spritecollide(self, self.game.walls, False) # recognize that the sprite collided
+            if hits: # if it collided
+                if self.vx > 0:  # and if it was going right
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:  # and it was going left
+                    self.x = hits[0].rect.right
+                self.vx = 0  # make it stop
+                self.rect.x = self.x
+        if dir == 'y':  # if the collision is vertical, i.e. from y
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:  # and if it was going down
+                    self.y = hits[0].rect.top - self.rect.height # it hit top of block, need to be at top minus our height
+                if self.vy < 0:  # and it was going upv
+                    self.y = hits[0].rect.bottom
+                self.vy = 0  # make it stop
+                self.rect.y = self.y
 
     def update(self):
-        self.rect.x = self.x * TILESIZE       # Finds location by pixel; i.e., 10 * 32 goes to 320 as top left hand corner
-        self.rect.y = self.y * TILESIZE
+        self.get_keys()
+        self.x += self.vx * self.game.dt     # Finds location by pixel; i.e., 10 * 32 goes to 320 as top left hand corner
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        self.collide_with_walls('y')
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
