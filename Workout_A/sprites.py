c4ff71f2436cf91c61814f43ff40089671d5f638
Mitch Stephens <mstephens@sandiego.edu>
2020-06-27 21:45:46 -0700

    Saved a mess...

--- Workout_A/sprites.py
+++ Workout_A/sprites.py
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
 from settings import *
 vec = pg.math.Vector2
 from tiledmap import collide_hit_rect
-=======
 import pygame as pg
 from settings import *
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-<<<<<<< HEAD
         self.image = game.player_img            # required for ALL classes; gets the sprite image
         self.rect = self.image.get_rect()       # required for ALL classes; gets the rectangle of the sprite image
         self.hit_rect = PLAYER_HIT_RECT
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)  # holds the center of the rectangle of the sprite
-        self.x = x
-        self.y = y
+        self.hit_rect.center = self.rect.center
         self.vel = vec(0, 0)
         self.pos = vec(x, y) * TILESIZE
         self.rot = 0
@@ -35,20 +29,8 @@ class Player(pg.sprite.Sprite):
             self.vel = vec(PLAYER_SPEED, 0).rotate(-self.rot)
         if keys[pg.K_DOWN] or keys[pg.K_s]:
             self.vel = vec(-PLAYER_SPEED / 2, 0).rotate(-self.rot)
-=======
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
-
-    def move(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
 
     def update(self):
-<<<<<<< HEAD
         self.get_keys()
         self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
         self.image = pg.transform.rotate(self.game.player_img, self.rot)    # rotates the actual img
@@ -56,34 +38,9 @@ class Player(pg.sprite.Sprite):
         self.rect.center = self.pos
         self.pos += self.vel * self.game.dt
         self.hit_rect.centerx = self.pos.x
-        self.collide_with_walls('x')
         self.hit_rect.centery = self.pos.y
-        self.collide_with_walls('y')
         self.rect.center = self.hit_rect.center
 
-    def collide_with_walls(self, dir):
-        if dir == 'x':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
-            if hits:
-                if self.vel.x > 0:
-                    self.pos.x = hits[0].rect.left - self.hit_rect.width / 2.0
-                if self.vel.x < 0:
-                    self.pos.x = hits[0].rect.right + self.hit_rect.width / 2.0
-                self.vel.x = 0
-                self.hit_rect.centerx = self.pos.x
-        if dir == 'y':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
-            if hits:
-                if self.vel.y > 0:
-                    self.pos.y = hits[0].rect.top - self.hit_rect.height / 2.0
-                if self.vel.y < 0:
-                    self.pos.y = hits[0].rect.bottom + self.hit_rect.height / 2.0
-                self.vel.y = 0
-                self.hit_rect.centery = self.pos.y
-=======
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
--- Workout_A/tiledmap.py
+++ Workout_A/tiledmap.py
@@ -1,37 +1,15 @@
-import pygame as pg
 import pytmx
 from settings import *
 
 def collide_hit_rect(one, two):
     return one.hit_rect.colliderect(two.rect)
 
-<<<<<<< HEAD
 class TiledMap:
     def __init__(self, filename):
         tm = pytmx.load_pygame(filename, pixelalpha=True)       # making a quicker variable to load the map
         self.width = tm.width * tm.tilewidth                    # getting the width of any TiledMap loaded in
         self.height = tm.height * tm.tileheight                 # getting the height of any TiledMap loaded in
         self.tmxdata = tm                                       # all of that data
-=======
-class Map:
-    def __init__(self, filename):
-        self.data = []
-        with open(filename, 'rt') as f:
-            for line in f:
-                self.data.append(line.strip())
-
-        self.tilewidth = len(self.data[0])
-        self.tileheight = len(self.data)
-        self.width = self.tilewidth * TILESIZE
-        self.height = self.tileheight * TILESIZE
-
-class TiledMap:
-    def __init__(self, filename):
-        tm = pytmx.load_pygame(filename, pixelalpha=True)
-        self.width = tm.width * tm.tilewidth
-        self.height = tm.height * tm.tileheight
-        self.tmxdata = tm
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
 
     def render(self, surface):
         ti = self.tmxdata.get_tile_image_by_gid
@@ -50,25 +28,16 @@ class TiledMap:
 
 class Camera:
     def __init__(self, width, height):
-<<<<<<< HEAD
         self.camera = pg.Rect(0, 0, width, height)      # use rectangle to track the camera
-=======
-        self.camera = pg.Rect(0, 0, width, height)
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
         self.width = width
         self.height = height
 
     def apply(self, entity):
-<<<<<<< HEAD
         return entity.rect.move(self.camera.topleft)        # gives new rectangle shifted by .topleft amount
-=======
-        return entity.rect.move(self.camera.topleft)
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
 
     def apply_rect(self, rect):
         return rect.move(self.camera.topleft)
 
-<<<<<<< HEAD
     def update(self, target):                               # target is the sprite we are following
         x = -target.rect.centerx + int(WIDTH / 2)           # go in the opposite direction of the player; half the screen size to keep the player centered
         y = -target.rect.centery + int(HEIGHT / 2)
@@ -79,15 +48,3 @@ class Camera:
         x = max(-(self.width - WIDTH), x)  # right; currently, -(2048-1024) or x
         y = max(-(self.height - HEIGHT), y)  # bottom; has to be greater than -(self.height - HEIGHT)
         self.camera = pg.Rect(x, y, self.width, self.height)    # adjusting what the rectangle is
-=======
-    def update(self, target):
-        x = -target.rect.centerx + int(WIDTH / 2)
-        y = -target.rect.centery + int(HEIGHT / 2)
-
-        # limit scrolling to map size
-        x = min(0, x)  # left
-        y = min(0, y)  # top
-        x = max(-(self.width - WIDTH), x)  # right
-        y = max(-(self.height - HEIGHT), y)  # bottom
-        self.camera = pg.Rect(x, y, self.width, self.height)
->>>>>>> d79e971bfc59112cb07698c087686def975adb0a
